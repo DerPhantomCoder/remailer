cf3fef4cbd5d658782a4f9c254fcd6e5b46d2b91
Phantom <coder@thephantomcoder.com>
2021-12-30 14:28:55 -0800

    Added unixfrom fixup because Python 3.6 doesn't emit From

--- remailer.py
+++ remailer.py
@@ -8,6 +8,7 @@ import logging
 import argparse
 import yaml
 from dbm import gnu as gdbm
+import time
 import sys
 import re
 import base64
@@ -226,7 +227,9 @@ class Remailer:
         # a header to indicate the message was logged.
         if self.log_messages == True:
             with open('/tmp/message_log', 'a') as f:
+                message.set_unixfrom('From {} {}'.format(self.to_addr.decode(),time.asctime(time.gmtime())))
                 f.write(message.as_string(unixfrom=True))
+                f.write('\r\n')
             self.message.add_header('X-Message-Logged', 'Yes')
 
         header_list = ('MIME-Version', 'Subject', 'Content-Language')
